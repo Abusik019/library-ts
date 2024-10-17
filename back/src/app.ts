119b33a19ce468c5e47aa8ccdeb830bb52d526da
Абужапар Гасанбеков <abusikgasanbekov@MacBook-Air-Abuzapar.local>
2024-10-17 20:14:28 +0300

    .

---
+++ back/src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from 'mongoose';
+import dotevn from 'dotenv';
+
+dotevn.config();
+
+const app = express();
+
+app.use(express.json());
+// app.user() router
+
+const startServer = async() => {
+    try{
+        // await mongoose.connect(process.env.MONGO_URL as string);
+        console.log('mongo was connected');
+    } catch(e) {
+        console.log(e);
+        process.exit(1);
+    }
+}
+
+startServer()
+
+export default app;
